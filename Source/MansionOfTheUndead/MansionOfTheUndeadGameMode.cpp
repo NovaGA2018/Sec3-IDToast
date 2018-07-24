1e68aa6892eb36e93f101466ad81e4ab4b0a2574
Mike Howlett <mah9882@rit.edu>
2018-07-24 10:38:52 -0400

    Converted project to a Third-Person default. Enjoy!

--- Source/MansionOfTheUndead/MansionOfTheUndeadGameMode.cpp
+++ Source/MansionOfTheUndead/MansionOfTheUndeadGameMode.cpp
@@ -1,17 +1,15 @@
 // Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
 
 #include "MansionOfTheUndeadGameMode.h"
-#include "MansionOfTheUndeadHUD.h"
 #include "MansionOfTheUndeadCharacter.h"
 #include "UObject/ConstructorHelpers.h"
 
 AMansionOfTheUndeadGameMode::AMansionOfTheUndeadGameMode()
-	: Super()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/FirstPersonCPP/Blueprints/FirstPersonCharacter"));
-	DefaultPawnClass = PlayerPawnClassFinder.Class;
-
-	// use our custom HUD class
-	HUDClass = AMansionOfTheUndeadHUD::StaticClass();
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPersonCPP/Blueprints/ThirdPersonCharacter"));
+	if (PlayerPawnBPClass.Class != NULL)
+	{
+		DefaultPawnClass = PlayerPawnBPClass.Class;
+	}
 }
